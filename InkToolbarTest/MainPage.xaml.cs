34c908aba803fc3a4e20ecf25ea1a35644643ad7
Michael S. Scherotter <mischero@microsoft.com>
2016-08-10 22:56:21 -0700

    Initial Commit

---
+++ InkToolbarTest/MainPage.xaml.cs
@@ -0,0 +1,226 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using Windows.Foundation;
+using Windows.UI;
+using Windows.UI.Core;
+using Windows.UI.Input.Inking;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Microsoft.Graphics.Canvas;
+using Microsoft.Graphics.Canvas.Effects;
+using Microsoft.Graphics.Canvas.UI.Xaml;
+
+namespace InkToolbarTest
+{
+    /// <summary>
+    ///     Ink Canvas, Drawing Canvas, and Canvas Control
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private InkSynchronizer _inkSynchronizer;
+        private bool _isErasing;
+        private Point _lastPoint;
+        private readonly List<InkStrokeContainer> _strokes = new List<InkStrokeContainer>();
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Loaded += MainPage_Loaded;
+        }
+
+
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            // 1. Activate custom drawing 
+            _inkSynchronizer = InkCanvas.InkPresenter.ActivateCustomDrying();
+
+            // 2. add use custom drawing when strokes are collected
+            InkCanvas.InkPresenter.StrokesCollected += InkPresenter_StrokesCollected;
+
+            // 3. Get the eraser button to handle custom dry ink and replace the erase all button with new logic
+            var eraser = InkToolbar.GetToolButton(InkToolbarTool.Eraser) as InkToolbarEraserButton;
+
+            if (eraser != null)
+            {
+                eraser.Checked += Eraser_Checked;
+                eraser.Unchecked += Eraser_Unchecked;
+            }
+
+            var flyout = FlyoutBase.GetAttachedFlyout(eraser) as Flyout;
+
+            if (flyout != null)
+            {
+                var button = flyout.Content as Button;
+
+                if (button != null)
+                {
+                    var newButton = new Button();
+                    newButton.Style = button.Style;
+                    newButton.Content = button.Content;
+
+                    newButton.Click += EraseAllInk;
+                    flyout.Content = newButton;
+                }
+            }
+        }
+
+        private void EraseAllInk(object sender, RoutedEventArgs e)
+        {
+            _strokes.Clear();
+
+            DrawingCanvas.Invalidate();
+        }
+
+        private void Eraser_Checked(object sender, RoutedEventArgs e)
+        {
+            var unprocessedInput = InkCanvas.InkPresenter.UnprocessedInput;
+
+            unprocessedInput.PointerPressed += UnprocessedInput_PointerPressed;
+            unprocessedInput.PointerMoved += UnprocessedInput_PointerMoved;
+            unprocessedInput.PointerReleased += UnprocessedInput_PointerReleased;
+            unprocessedInput.PointerExited += UnprocessedInput_PointerExited;
+            unprocessedInput.PointerLost += UnprocessedInput_PointerLost;
+
+            InkCanvas.InkPresenter.InputProcessingConfiguration.Mode = InkInputProcessingMode.None;
+        }
+
+        private void Eraser_Unchecked(object sender, RoutedEventArgs e)
+        {
+            var unprocessedInput = InkCanvas.InkPresenter.UnprocessedInput;
+
+            unprocessedInput.PointerPressed -= UnprocessedInput_PointerPressed;
+            unprocessedInput.PointerMoved -= UnprocessedInput_PointerMoved;
+            unprocessedInput.PointerReleased -= UnprocessedInput_PointerReleased;
+            unprocessedInput.PointerExited -= UnprocessedInput_PointerExited;
+            unprocessedInput.PointerLost -= UnprocessedInput_PointerLost;
+
+            InkCanvas.InkPresenter.InputProcessingConfiguration.Mode = InkInputProcessingMode.Inking;
+        }
+
+        private void UnprocessedInput_PointerMoved(InkUnprocessedInput sender, PointerEventArgs args)
+        {
+            if (!_isErasing)
+            {
+                return;
+            }
+
+            var invalidate = false;
+
+            foreach (var item in _strokes.ToArray())
+            {
+                var rect = item.SelectWithLine(_lastPoint, args.CurrentPoint.Position);
+
+                if (rect.IsEmpty)
+                {
+                    continue;
+                }
+
+                if (rect.Width * rect.Height > 0)
+                {
+                    _strokes.Remove(item);
+
+                    invalidate = true;
+                }
+            }
+
+            _lastPoint = args.CurrentPoint.Position;
+
+            args.Handled = true;
+
+            if (invalidate)
+            {
+                DrawingCanvas.Invalidate();
+            }
+        }
+
+        private void UnprocessedInput_PointerLost(InkUnprocessedInput sender, PointerEventArgs args)
+        {
+            if (_isErasing)
+            {
+                args.Handled = true;
+            }
+
+            _isErasing = false;
+        }
+
+        private void UnprocessedInput_PointerExited(InkUnprocessedInput sender, PointerEventArgs args)
+        {
+            if (_isErasing)
+            {
+                args.Handled = true;
+            }
+
+            _isErasing = true;
+        }
+
+        private void UnprocessedInput_PointerPressed(InkUnprocessedInput sender, PointerEventArgs args)
+        {
+            _lastPoint = args.CurrentPoint.Position;
+
+            args.Handled = true;
+
+            _isErasing = true;
+        }
+
+        private void UnprocessedInput_PointerReleased(InkUnprocessedInput sender, PointerEventArgs args)
+        {
+            if (_isErasing)
+            {
+                args.Handled = true;
+            }
+
+            _isErasing = false;
+        }
+
+        private void InkPresenter_StrokesCollected(InkPresenter sender, InkStrokesCollectedEventArgs args)
+        {
+            var strokes = _inkSynchronizer.BeginDry();
+
+            var container = new InkStrokeContainer();
+
+            container.AddStrokes(from item in strokes
+                select item.Clone());
+
+            _strokes.Add(container);
+
+            _inkSynchronizer.EndDry();
+
+            DrawingCanvas.Invalidate();
+        }
+
+        private void DrawCanvas(CanvasControl sender, CanvasDrawEventArgs args)
+        {
+            DrawInk(args.DrawingSession);
+        }
+
+        private void DrawInk(CanvasDrawingSession session)
+        {
+            foreach (var item in _strokes)
+            {
+                var strokes = item.GetStrokes();
+
+                using (var list = new CanvasCommandList(session))
+                {
+                    using (var listSession = list.CreateDrawingSession())
+                    {
+                        listSession.DrawInk(strokes);
+                    }
+
+                    using (var shadowEffect = new ShadowEffect
+                    {
+                        ShadowColor = Colors.DarkRed,
+                        Source = list
+                    })
+                    {
+                        session.DrawImage(shadowEffect, new Vector2(2, 2));
+                    }
+                }
+
+                session.DrawInk(strokes);
+            }
+        }
+    }
+}
